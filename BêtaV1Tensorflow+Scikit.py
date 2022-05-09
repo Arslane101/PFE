ac6adc3fcc3906a923913bab1775441129721600
Arslane Rahal <rahalabdelhamid@gmail.com>
2022-05-09 17:20:02 +0100

    Update BêtaV1Tensorflow+Scikit.py

--- BêtaV1Tensorflow+Scikit.py
+++ BêtaV1Tensorflow+Scikit.py
@@ -33,18 +33,20 @@ def ListRelevant(matrix,n_items,ind):
         if(matrix.iloc[ind,i]==1):
             relevants.append(i)
     return relevants   
-def GenInputTargetUser(matrix,n_items,ind):
-    Input = list()
-    Target = list()
-    for i, j in zip(range(len(ListRelevant(matrix,n_items,nb))), ListRelevant(matrix,n_items,nb)):
-     copy = np.array(matrix[ind,:],copy=True)
-     copy[j]=0
-     Target.append(j)
-     Input.append(copy)   
+def GenInputTargetUser(pivot,n_items,ind):
+    i=0
+    Input = np.zeros((nbrel,n_items))
+    Target = np.zeros((nbrel))
+    for nb in train:
+     for j in  ListRelevant(pivot,n_items,nb):
+        Input[i] = np.array(pivot.iloc[nb,:],copy=True)
+        Input[i,j]=0
+        Target[i]=j
+        i+=1 
     return Input,Target
 """Création des inputs et targets du RDN"""
 gc.enable()
-ratings = ChargerDataset("../input/movies/ratings.csv",4)
+ratings = ChargerDataset("ratings.csv",4)
 pivot = ratings.pivot_table(index=['userId'],columns=['movieId'],values='rating',fill_value=0)
 ratings = None
 n_users = pivot.index.unique().shape[0]
@@ -66,33 +68,43 @@ for nb in train:
         InputTr[i,j]=0
         TargetTr[i]=j
         i+=1
+InputTe = np.zeros((nbrel,n_items))
+TargetTe = np.zeros((nbrel))
+for nb in test:
+  for j in  ListRelevant(pivot,n_items,nb):
+        InputTe[i] = np.array(pivot.iloc[nb,:],copy=True)
+        InputTe[i,j]=0
+        TargetTe[i]=j
+        i+=1
 
 model = Sequential()
 model.add(Input(shape=(nbrel,n_items)))
-model.add(Dense(700, activation='selu'))
+model.add(Dense(500, activation='elu'))
 model.add(Dropout(rate=0.2))
-model.add(Dense(500,activation='selu'))
+model.add(Dense(250,activation='elu'))
 model.add(Dropout(rate=0.2))
 model.add(Dense(n_items,activation='softmax'))
 model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.summary()
-history = model.fit(InputTr,TargetTr,epochs=50,batch_size=150)
-"""
+history = model.fit(InputTr,TargetTr,epochs=200,batch_size=250)
+
 # list all data in history
 print(history.history.keys())
 # summarize history for accuracy
 plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
 plt.title('model accuracy')
 plt.ylabel('accuracy')
 plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
+plt.legend(['train'], loc='upper left')
 plt.show()
 # summarize history for loss
 plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
 plt.title('model loss')
 plt.ylabel('loss')
 plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-plt.show()"""
+plt.legend(['train'], loc='upper left')
+plt.show()
+
+print("Evaluate on test data")
+results = model.evaluate(InputTe, TargetTe, batch_size=128)
+print("test loss, test acc:", results)
