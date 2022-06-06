eb9bdc7d6b771c562ad303be4191ed006d7bd66b
Arslane Rahal <rahalabdelhamid@gmail.com>
2022-06-06 10:52:03 +0100

    Update NCCFDemo.py

--- NCCFDemo.py
+++ NCCFDemo.py
@@ -23,20 +23,19 @@ def Relevant(matrix):
             if(matrix.iloc[i,j]==1) and j not in relevants:
               relevants.append(j)
     return relevants   
-
 @st.cache(suppress_st_warning=True)
-def Prediction(number):
-   usertable = np.array(pivot.iloc[int(number)-1,:],copy=True)
-   testUser = usertable.reshape(1,usertable.shape[0])
-   results = model.predict(testUser)
-   results = np.argsort(results.reshape(testUser.shape[1]))[::-1]
-   return results
-def PlotResults(results):
+def WriteMovieList():
+    number = st.session_state['select']
+    num = st.session_state['numrec2']
+    usertable = np.array(pivot.iloc[int(number)-1,:],copy=True)
+    testUser = usertable.reshape(1,usertable.shape[0])
+    results = model.predict(testUser)
+    results = np.argsort(results.reshape(testUser.shape[1]))[::-1]
     n=96
     i=1
     recalls = []
     precisions = []
-    rev = ListRelevant(pivot,pivot.shape[1],int(number)-1)
+    rev = ListRelevant(pivot,pivot.shape[1],int(st.session_state['select'])-1)
     while(i<n):
       rec = 0
       prec = 0
@@ -50,8 +49,42 @@ def PlotResults(results):
       i+=5
       precisions.append(prec)
       recalls.append(rec)    
-    st.line_chart(data=recalls)
-    st.line_chart(data=precisions)
+    temp = results[:(int(num))]
+    movieslist = list()
+    for i in temp:
+      movieslist.append(movies[movies['movieId']==list_movieids[i]]['Title'])
+    return precisions,recalls,movieslist
+@st.cache(suppress_st_warning=True)
+def PredictionNewUser():
+    number = st.session_state["slider1"]
+    numberec = st.session_state["numrec"]
+    usertable = randomusers[int(number)-1,:]
+    rev = ListRel(usertable)
+    testUser = usertable.reshape(1,usertable.shape[0])
+    results = model.predict(testUser)
+    results = np.argsort(results.reshape(testUser.shape[1]))[::-1]
+    temp = results[:(int(numberec))]
+    movieslist = list()
+    for i in temp:
+     movieslist.append(movies[movies['movieId']==list_movieids[i]]['Title'])
+    n=96
+    i=1
+    recalls = []
+    precisions = []
+    while(i<n):
+      rec = 0
+      prec = 0
+      hr=0
+      temp = results[:i]
+      for k in range(len(temp)):
+        if  temp[k] in rev:
+         hr+=1
+      prec =  (hr)/i
+      rec =  (hr)/len(rev)
+      i+=5
+      precisions.append(prec)
+      recalls.append(rec)    
+    return precisions,recalls,movieslist
 ratings = pd.read_csv("ml-100k/filteredratings.csv",delimiter=";",parse_dates=['timestamp'])
 pivot = ratings.pivot_table(index=['userId'],columns=['movieId'],values='rating',fill_value=0)
 randomusers = np.loadtxt("RandomUsers.txt")
@@ -62,49 +95,23 @@ st.set_page_config(
     page_title="NCCF",
     page_icon="👋",
 )
+
+
 st.text("Si vous êtes un nouvel utilisateur : ")
-options = st.selectbox("Séléctionnez le numéro de l'utilisateur test"
-,['1','2','3','4','5'])
-first = st.button('Valider et Lancer la recommandation')
-if(first):
-    numberec = st.slider("Séléctionnez le nombre de recommandations à afficher",1,96)
-    usertable = randomusers[int(options)-1,:]
-    rev = ListRel(usertable)
-    testUser = usertable.reshape(1,usertable.shape[0])
-    results = model.predict(testUser)
-    results = np.argsort(results.reshape(testUser.shape[1]))[::-1]
-    execres = PrintListMovies(numberec,results)
-    st.text(execres)
-    n=96
-    i=1
-    recalls = []
-    precisions = []
-    while(i<n):
-      rec = 0
-      prec = 0
-      hr=0
-      temp = results[:i]
-      for k in range(len(temp)):
-        if  temp[k] in rev:
-         hr+=1
-      prec =  (hr)/i
-      rec =  (hr)/len(rev)
-      i+=5
-      precisions.append(prec)
-      recalls.append(rec)    
-    st.line_chart(data=precisions)
-    st.line_chart(data=recalls)
-
-
+options = st.selectbox("Séléctionnez votre numéro d'utilisateur"
+,['1','2','3','4','5'],key='slider1',on_change=PredictionNewUser)
+numberec2 = st.slider("Séléctionnez le nombre de recommandations à afficher",1,96,key='numrec',on_change=PredictionNewUser)
+Results = PredictionNewUser()
+st.text(Results[2])
+st.line_chart(data=Results[0])
+st.line_chart(data=Results[1])
 st.text("Si vous êtes un utilisateur existant :")
-number = st.number_input("Saisissez votre numéro d'utilisateur",1,943)
-numberec = st.slider("Séléctionnez le nombre de recommandations à afficher",1,96)
-results = Prediction(number)
-temp = results[:(int(numberec))]
-movieslist = list()
-for i in temp:
-    movieslist.append(movies[movies['movieId']==list_movieids[i]]['Title'])
-st.text(movieslist)
-PlotResults(results)
+number = st.number_input("Saisissez votre numéro d'utilisateur",1,943,key='select',on_change=WriteMovieList)
+numberec = st.slider("Séléctionnez le nombre de recommandations à afficher",1,96,key='numrec2',on_change=WriteMovieList)
+Results = WriteMovieList()
+st.text(Results[2])
+st.line_chart(data=Results[0])
+st.line_chart(data=Results[1])
+
 
 
