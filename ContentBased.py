301788fb8bb884c373acb26a282d5be015e6b2dc
Arslane Rahal <rahalabdelhamid@gmail.com>
2022-06-08 10:33:48 +0100

    ajout content based

--- ContentBased.py
+++ ContentBased.py
@@ -1,3 +1,5 @@
+from cgi import test
+import imp
 import pandas as pd
 import matplotlib as plt
 import numpy as np
@@ -8,32 +10,15 @@ def ListRel(array):
             relevants.append(i)
     return relevants 
 
-ratings = pd.read_csv("specificratings.csv",delimiter=";",parse_dates=["timestamp"])
-movies = pd.read_csv("ml-100k/filmsenrichis.csv",delimiter=";")
-ratings = pd.merge(ratings,movies,on="movieId")
-ratings_df = pd.DataFrame(ratings.groupby('Title')['rating'].mean())
-ratings_df.rename(columns={'rating': 'average_rating'}, inplace=True)
-ratings_df['num_of_ratings'] = pd.DataFrame(ratings.groupby('Title')['rating'].count())
-pivot = ratings.pivot_table(index=['userId'],columns=['Title'],values='rating')
-list_movies = movies['Title'].values
-def SimilarMovies(film, min_num_reviews):
-    list_ratings = pivot[film]
-    similar_to_film =pivot.corrwith(list_ratings)
-    corr_film_x = pd.DataFrame(similar_to_film, columns=['Correlation'])
-    corr_film_x.dropna(inplace=True)
-    corr_film_x = corr_film_x.join(ratings_df['num_of_ratings'])
-    new_corr_film_x = corr_film_x[corr_film_x['num_of_ratings'] >= min_num_reviews]
-    return new_corr_film_x.sort_values('Correlation',ascending=False)
-def GenAllRecommendations(userratings,minreviews):
-    list_all_movies = ListRel(userratings)
-    moviestitles = list()
-    for id in list_all_movies:
-        temp = SimilarMovies(list_movies[id],minreviews)
-        if(len(temp.index)>=5):
-          for i in range(5):
-            moviestitles.append(temp.index[i+1])
-    return moviestitles
-
-
-Users = np.loadtxt("RandomUsers.txt")
-print(GenAllRecommendations(Users[0],50))
+ratings = pd.read_csv("Content-based/specificratings.csv",delimiter=";",parse_dates=["timestamp"])
+ratings = ratings.drop('timestamp',1)
+movies = pd.read_csv("Content-based/movies.csv",delimiter=";")
+testUser = ratings[ratings["userId"]==1]
+usermovies = movies[movies['movieId'].isin(testUser["movieId"].tolist())]
+usermovies = usermovies.reset_index(drop=True)
+usermovies  =usermovies.drop('movieId',1)
+print(pd.DataFrame(testUser['rating']))
+transpose = usermovies.transpose()
+print(transpose.head())
+userprofile = transpose.dot(pd.DataFrame(testUser['rating']))
+print(userprofile)
