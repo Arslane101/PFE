85ee5e4d9d13735dc50bed966ffb484da3ff5ead
Arslane Rahal <rahalabdelhamid@gmail.com>
2022-06-08 16:33:57 +0100

    updates

--- ContentBased.py
+++ ContentBased.py
@@ -5,25 +5,41 @@ from matplotlib.pyplot import axis
 import pandas as pd
 import matplotlib as plt
 import numpy as np
-def ListRel(array):
-    relevants = []
-    for i in range(len(array)):
-        if(array[i]>=4):
-            relevants.append(i)
-    return relevants 
-
-ratings = pd.read_csv("Content-based/specificratings.csv",delimiter=";",parse_dates=["timestamp"])
-ratings = ratings.drop('timestamp',1)
-movies = pd.read_csv("Content-based/movies.csv",delimiter=";")
-testUser = ratings[ratings["userId"]==1]
-usermovies = movies[movies['movieId'].isin(testUser["movieId"].tolist())]
-usermovies = usermovies.reset_index(drop=True)
-usermovies  =usermovies.drop('movieId',1)
-userprofile = usermovies.transpose().dot(testUser['rating'].reset_index(drop=True))
-genreTable = movies.set_index(movies['movieId'])
-genreTable = genreTable.drop('movieId', 1)
-print(genreTable.shape)
-print(userprofile.head())
-recommendation = ((genreTable*userprofile).sum(axis=1))/(userprofile.sum())
-sorted = recommendation.sort_values(ascending=False)
-print(sorted.head())
+def AllMoviesbyCountry(country):
+    items = pd.read_csv("ml-100k/filmsenrichis.csv",delimiter=";")
+    movies = pd.read_csv("ml-100k/dbpediamovies.csv",delimiter=";")
+    specificmovies = movies[movies['country'].isin(country)]['name'].unique()
+    uniqueids = items[items['SPARQLTitle'].isin(specificmovies)]['movieId'].unique()
+    return uniqueids
+def ContentBasedNoLOD(userId):
+    ratings = pd.read_csv("Content-based/specificratings.csv",delimiter=";",parse_dates=["timestamp"])
+    ratings = ratings.drop('timestamp',1)
+    movies = pd.read_csv("Content-based/movies.csv",delimiter=";")
+    moviecopy = pd.read_csv("Content-based/filmsenrichis.csv",delimiter=";")
+    testUser = ratings[ratings["userId"]==userId]
+    usermovies = movies[movies['movieId'].isin(testUser["movieId"].tolist())]
+    usermovies = usermovies.reset_index(drop=True)
+    usermovies  =usermovies.drop('movieId',1)
+    userprofile = usermovies.transpose().dot(testUser['rating'].reset_index(drop=True))
+    genreTable = movies.set_index(movies['movieId'])
+    genreTable = genreTable.drop('movieId', 1)
+    recommendation = ((genreTable*userprofile).sum(axis=1))/(userprofile.sum())
+    sorted = recommendation.sort_values(ascending=False)
+    listmovies = moviecopy.loc[moviecopy['movieId'].isin(sorted.head(20).keys())]
+    return listmovies
+def ContentBasedLOD(userId):
+    ratings = pd.read_csv("Content-based/specificratings.csv",delimiter=";",parse_dates=["timestamp"])
+    ratings = ratings.drop('timestamp',1)
+    movies = pd.read_csv("Content-based/moviesgenrescountries.csv",delimiter=";")
+    moviecopy = pd.read_csv("Content-based/filmsenrichis.csv",delimiter=";")
+    testUser = ratings[ratings["userId"]==userId]
+    usermovies = movies[movies['movieId'].isin(testUser["movieId"].tolist())]
+    usermovies = usermovies.reset_index(drop=True)
+    usermovies  =usermovies.drop('movieId',1)
+    userprofile = usermovies.transpose().dot(testUser['rating'].reset_index(drop=True))
+    genreTable = movies.set_index(movies['movieId'])
+    genreTable = genreTable.drop('movieId', 1)
+    recommendation = ((genreTable*userprofile).sum(axis=1))/(userprofile.sum())
+    sorted = recommendation.sort_values(ascending=False)
+    listmovies = moviecopy.loc[moviecopy['movieId'].isin(sorted.head(20).keys())]
+    return listmovies
