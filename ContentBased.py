7b12e0eccc35757a7c9f627e5f763e4e67e0d921
Arslane Rahal <rahalabdelhamid@gmail.com>
2022-06-08 21:40:16 +0100

    Update ContentBased.py

--- ContentBased.py
+++ ContentBased.py
@@ -1,19 +1,25 @@
-from cgi import test
-import imp
-from cv2 import sort
-from matplotlib.pyplot import axis
-import pandas as pd
-import matplotlib as plt
+
+import random
 import numpy as np
+import pandas as pd
+
+
 def AllMoviesbyCountry(country):
     items = pd.read_csv("ml-100k/filmsenrichis.csv",delimiter=";")
     movies = pd.read_csv("ml-100k/dbpediamovies.csv",delimiter=";")
     specificmovies = movies[movies['country'].isin(country)]['name'].unique()
     uniqueids = items[items['SPARQLTitle'].isin(specificmovies)]['movieId'].unique()
     return uniqueids
-def ContentBasedNoLOD(userId):
+def ListRelevant(userId,th):
     ratings = pd.read_csv("Content-based/specificratings.csv",delimiter=";",parse_dates=["timestamp"])
-    ratings = ratings.drop('timestamp',1)
+    testUser = ratings[ratings["userId"]==userId]
+    testUser = testUser.reset_index(drop=True)
+    relevant = list()
+    for i in range(testUser.shape[0]):
+        if(testUser['rating'][i]>=th):
+            relevant.append(testUser['movieId'][i])
+    return relevant
+def ContentBasedNoLOD(userId):
     movies = pd.read_csv("Content-based/movies.csv",delimiter=";")
     moviecopy = pd.read_csv("Content-based/filmsenrichis.csv",delimiter=";")
     testUser = ratings[ratings["userId"]==userId]
@@ -28,10 +34,7 @@ def ContentBasedNoLOD(userId):
     listmovies = moviecopy.loc[moviecopy['movieId'].isin(sorted.head(20).keys())]
     return listmovies
 def ContentBasedLOD(userId):
-    ratings = pd.read_csv("Content-based/specificratings.csv",delimiter=";",parse_dates=["timestamp"])
-    ratings = ratings.drop('timestamp',1)
     movies = pd.read_csv("Content-based/moviesgenrescountries.csv",delimiter=";")
-    moviecopy = pd.read_csv("Content-based/filmsenrichis.csv",delimiter=";")
     testUser = ratings[ratings["userId"]==userId]
     usermovies = movies[movies['movieId'].isin(testUser["movieId"].tolist())]
     usermovies = usermovies.reset_index(drop=True)
@@ -41,5 +44,39 @@ def ContentBasedLOD(userId):
     genreTable = genreTable.drop('movieId', 1)
     recommendation = ((genreTable*userprofile).sum(axis=1))/(userprofile.sum())
     sorted = recommendation.sort_values(ascending=False)
-    listmovies = moviecopy.loc[moviecopy['movieId'].isin(sorted.head(20).keys())]
+    listmovies = moviecopy.loc[moviecopy['movieId'].isin(sorted.head(96).keys())]
     return listmovies
+ratings = pd.read_csv("Content-based/specificratings.csv",delimiter=";",parse_dates=["timestamp"])
+moviecopy = pd.read_csv("Content-based/filmsenrichis.csv",delimiter=";")
+ratings = ratings.drop('timestamp',1)
+n=96
+totalprec = list()
+totalrec = list()
+list_users = random.sample(ratings['userId'].unique().tolist(),20)
+j=0
+for j in range(len(list_users)) :
+ rev = ListRelevant(list_users[j],3)
+ hr=0
+ recalls = list()
+ precisions = list()
+ recalls.append(list_users[j])
+ precisions.append(list_users[j])
+ recommended = ContentBasedLOD(list_users[j])
+ recommended = recommended.reset_index(drop=True)
+ if(len(rev)!=0):
+  recalls.append(len(rev))
+  precisions.append(len(rev)) 
+  temp= recommended.head(10)
+  for k in range(temp.shape[0]):
+     if(temp['movieId'][k] in rev):
+        hr+=1
+  prec = (hr)/10
+  rec =  (hr)/len(rev)
+  precisions.append(prec)
+  recalls.append(rec)
+  j+=1
+  print(j)
+ totalprec.append(np.asarray(precisions))
+ totalrec.append(np.asarray(recalls))
+np.savetxt("AllPrecisions.txt", np.vstack(totalprec).astype(float),fmt='%.2f')
+np.savetxt("AllRecalls.txt",np.vstack(totalrec).astype(float),fmt='%.2f')
