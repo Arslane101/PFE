3f1371e09d8df107b28986328d7c50658598f2f6
Arslane Rahal <rahalabdelhamid@gmail.com>
2022-07-16 11:51:37 +0100

    yes

--- matchingmovies.py
+++
@@ -1,9 +0,0 @@
-from unittest import result
-import pandas as pd
-import numpy as np
-import jellyfish 
-
-movies = pd.read_csv("ml-100k/dbpediamovies.csv",delimiter=";")
-results = pd.read_csv("ml-100k/resultssparql.csv",delimiter=";")
-newdata = pd.merge(movies,results[["name","budget"]],on='name')
-newdata.to_csv("results.csv")
