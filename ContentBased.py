653c46febf9834bf06493f4327adc7983cd9d956
Arslane Rahal <rahalabdelhamid@gmail.com>
2022-09-10 03:38:37 +0100

    Update ContentBased.py

--- ContentBased.py
+++ ContentBased.py
@@ -1,103 +1,76 @@
-
-from cgi import test
-import random
-import numpy as np
+from traceback import print_tb
+from unittest import result
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from itertools import combinations
+import seaborn as sns
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from numpy.lib.npyio import itertools
+from sklearn.model_selection import train_test_split
 
-def AllMoviesbyDirector(director):
-    items = pd.read_csv("ml-100k/filmsenrichis.csv",delimiter=";")
-    movies = pd.read_csv("ml-100k/dbpediamovies.csv",delimiter=";")
-    specificmovies = movies[movies['director'].isin(director)]['name'].unique()
-    uniqueids = items[items['SPARQLTitle'].isin(specificmovies)]['movieId'].unique()
-    return uniqueids
-def AllMoviesbyCountry(country):
-    specificmovies = movies[movies['country'].isin(country)]['name'].unique()
-    uniqueids = items[items['SPARQLTitle'].isin(specificmovies)]['movieId'].unique()
-    return uniqueids
-def EnrichissementparPays():
-    movies = pd.read_csv("Content-based/moviesgenrescountries.csv",delimiter=";")
-    specificmovies = pd.read_csv("ml-100k/dbpediamovies.csv",delimiter=";")
-    directors = specificmovies['director'].unique().tolist()
-    for i in directors:
-        movies.insert(movies.shape[1],i,0)
-    i=0
-    for j in directors:
-        for i in range(movies.shape[0]):
-            ids = AllMoviesbyDirector([j])
-            if(movies["movieId"][i] in ids):
-                movies[j][i]=1
-    movies.to_csv("euh.csv")
-    return movies    
-def ListRelevant(userId,th):
-    ratings = pd.read_csv("Content-based/specificratings.csv",delimiter=";",parse_dates=["timestamp"])
-    testUser = ratings[ratings["userId"]==userId]
-    testUser = testUser.reset_index(drop=True)
-    relevant = list()
-    for i in range(testUser.shape[0]):
-        if(testUser['rating'][i]>=th):
-            relevant.append(testUser['movieId'][i])
-    return relevant
-def ContentBasedNoLOD(userId):
-    movies = pd.read_csv("Content-based/movies.csv",delimiter=";")
-    testUser = ratings[ratings["userId"]==userId]
-    testUser = testUser.sort_values(by=['movieId'])
-    usermovies = movies[movies['movieId'].isin(testUser["movieId"].tolist())]
-    usermovies = usermovies.reset_index(drop=True)
-    userprofile = usermovies.transpose().dot(testUser['rating'].reset_index(drop=True))
-    genreTable = movies.set_index(movies['movieId'])
-    genreTable = genreTable.drop('movieId', 1)
-    recommendation = ((genreTable*userprofile).sum(axis=1))/(userprofile.sum())
-    sorted = recommendation.sort_values(ascending=False)
-    return sorted.head(96).keys()
-def ContentBasedLOD(userId):
-    movies = EnrichissementparPays()
-    print(movies.head(15))
-    testUser = ratings[ratings["userId"]==userId]
-    testUser = testUser.sort_values(by=['movieId'])
-    usermovies = movies[movies['movieId'].isin(testUser["movieId"].tolist())]
-    usermovies = usermovies.reset_index(drop=True)
-    usermovies  =usermovies.drop('movieId',1)
-    userprofile = usermovies.transpose().dot(testUser['rating'].reset_index(drop=True))
-    genreTable = movies.set_index(movies['movieId'])
-    genreTable = genreTable.drop('movieId', 1)
-    recommendation = ((genreTable*userprofile).sum(axis=1))/(userprofile.sum())
-    sorted = recommendation.sort_values(ascending=False)
-    return sorted.head(96).keys()
-items = pd.read_csv("ml-100k/filmsenrichis.csv",delimiter=";")
-movies = pd.read_csv("ml-100k/dbpediamovies.csv",delimiter=";")
-ratings = pd.read_csv("Content-based/specificratings.csv",delimiter=";",parse_dates=["timestamp"])
-moviecopy = pd.read_csv("Content-based/filmsenrichis.csv",delimiter=";")
-EnrichissementparPays()
-"""ratings = ratings.drop('timestamp',1)
+
+def contentbased(user,movies,ratings):
+        movies.dropna(subset=['movie_info'], inplace=True)
+        movies = movies.reset_index()
+        userLID = ratings['userId'].unique()
+
+        tf = TfidfVectorizer(stop_words='english')
+        tfidf_matrix_item = tf.fit_transform(movies['movie_info'])
+        userRate = ratings[ratings['userId'] == user]
+        relRating = userRate[userRate['rating'] >= 3]
+        userM = movies[movies['rotten_tomatoes_link'].isin(relRating['movieId'])]            
+        featureMat = pd.DataFrame(tfidf_matrix_item.todense(),
+                                        columns=tf.get_feature_names_out(),
+                                        index=movies.rotten_tomatoes_link)
+        featureMatU = featureMat[featureMat.index.isin(userRate['movieId'])]
+        featureMatU = (pd.DataFrame((featureMatU.mean()),
+                                        columns=['similarity'])).transpose()       
+        cosine_sim = cosine_similarity(featureMatU, tfidf_matrix_item)
+        cosine_sim_df = pd.DataFrame(columns=['movieId','similarity'])
+        cosine_sim = cosine_sim.T
+        for i in range(cosine_sim.shape[0]):
+                cosine_sim_df.loc[len(cosine_sim_df.index)]= [movies['rotten_tomatoes_link'][i],cosine_sim[i]]
+        return relRating.movieId.unique(),cosine_sim_df
+                
+ratings = pd.read_csv("normalizedreviews.csv",delimiter=";",parse_dates=['review_date'],infer_datetime_format=True)
+ratings.dropna(axis=0, subset=['rating'], inplace=True)
+#ratings.fillna(0)
+ratings.drop_duplicates(subset=['movieId', 'userId'], inplace=True)
+ratings = ratings.reset_index()
+movies = pd.read_csv('movies.csv', delimiter=';')
+
+j=0
 n=96
 totalprec = list()
 totalrec = list()
-list_users = ratings['userId'].unique().tolist()
-print(len(list_users))
-for j in list_users :
- rev = ListRelevant(j,3)
+totalf = list()
+for j in range(50):
  print(j)
- if(len(rev)>15):
-  recalls = list()
-  precisions = list()
-  recalls.append(j)
-  precisions.append(j)
-  recommended = ContentBasedLOD(j)
-  recalls.append(len(rev))
-  precisions.append(len(rev))
-  i=10
-  while(i<50): 
-   hr=0
-   temp= recommended[:i]
-   for k in range(len(temp)):
-     if(temp[k] in rev):
-        hr+=1
-   prec = (hr)/i
-   rec =  (hr)/len(rev)
-   precisions.append(prec)
-   recalls.append(rec)
-   i+=10
+ recalls = list()
+ precisions = list()
+ recalls.append(j)
+ precisions.append(j)
+ i=1
+ rev,results = contentbased(ratings.userId.unique()[j],movies,ratings)
+ results = results.sort_values(by=['similarity'],ascending=False)
+ results = results.reset_index()
+ if(len(rev)!=0):
+  while(i<n):   
+    hr=0
+    temp = results.loc[0:i-1,:] 
+    for k in range(len(temp)):
+         if  temp['movieId'][k] in rev:
+          hr+=1
+    prec = (hr)/i
+    rec =  (hr)/len(rev) 
+    precisions.append(prec)
+    recalls.append(rec)
+    i+=5
   totalprec.append(np.asarray(precisions))
   totalrec.append(np.asarray(recalls))
 np.savetxt("AllPrecisions.txt", np.vstack(totalprec).astype(float),fmt='%.2f')
-np.savetxt("AllRecalls.txt",np.vstack(totalrec).astype(float),fmt='%.2f')"""
+np.savetxt("AllRecalls.txt",np.vstack(totalrec).astype(float),fmt='%.2f')
+
+
